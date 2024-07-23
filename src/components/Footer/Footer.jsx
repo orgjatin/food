16f960106435f974c7ef46a9972eb8e74dc3d5dc
orgjatin <org.jatin@gmail.com>
2024-07-23 18:10:49 +0530

    responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import './Footer.css'
+import {assets} from '../../assets/assets'
 
 const Footer = () => {
   return (
-    <div className='footer'>
-        Copyright © 2024 All rights reserved
+    <div className='footer' id='footer'>
+        
+        <div className="footer-content">
+          <div className="footer-content-left">
+            <img src={assets.logo} alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro itaque dolorem cupiditate omnis quas vitae magnam qui consequuntur quod in, possimus quis sapiente, veritatis placeat ea nulla et cumque vero?</p>
+            <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+            </div>
+
+          </div>
+
+          <div className="footer-content-center">
+            
+          <h2>Company</h2>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+
+          </div>
+          <div className="footer-content-right">
+            <h2>Get in Touch</h2>
+            <ul>
+              <li>+919876543210</li>
+              <li>contact@tomato.com</li>
+            </ul>
+          </div>
+        </div>
+      
+        <p className="footer-copyright">
+          Copyright 2024 all rights reserved
+        </p>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,9 +9,8 @@ const Navbar = () => {
         <img src={assets.logo} alt="" className="logo" />
         <ul className="navbar-menu">
             <li onClick={()=>setMenu("home")} className={menu === "home"?"active":""}>Home</li>
-            <li onClick={()=>setMenu("menu")} className={menu === "menu"?"active":""}>Menu</li>
-            <li onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app"?"active":""}>Mobile-app</li>
-            <li onClick={()=>setMenu("contact-us")} className={menu === "contact-us"?"active":""}>Contact Us</li>
+            <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu === "menu"?"active":""}>Menu</a>
+            <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu === "contact-us"?"active":""}>Contact Us</a>
         </ul>
         <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
